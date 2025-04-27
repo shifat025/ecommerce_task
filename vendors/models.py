71242342ba07f243174001be98df4b22eccb38a1
shifat025 <shifat58201@gmail.com>
2025-04-27 19:59:50 +0600

    fix one issue

--- vendors/models.py
+++ vendors/models.py
@@ -4,7 +4,6 @@ import uuid
 # Create your models here.
 
 class Vendor(models.Model):
-    """Represents a vendor in the system."""
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='vendor_profile')
     store_name = models.CharField(max_length=255)
